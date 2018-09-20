e818a21e002840220d05914321e66a08cd55a584
Usuario <Usuario@10.22.148.84>
2018-09-20 11:39:40 +0100

    Kata1 - V2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,10 +1,14 @@
 package kata1;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person a = new Person("Alvaro",new Date(98,7,10));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1998,8,10);
+        Person a = new Person("Alvaro", date);
         int años = a.getAge();
         System.out.println(a.getName() + " ha nacido en: " + a.getBirthdate());
         System.out.println("Sa edad es: " + años);
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,12 +1,15 @@
 package kata1;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 
  public class Person {
     private final String name;
-    private final Date birthdate;
+    private final Calendar birthdate;
+    private final long MS_per_year = (long) (1000*60*60*24*365.25);
    
-    public Person (String name, Date birthdate){
+    public Person (String name, Calendar birthdate){
         this.name = name;
         this.birthdate= birthdate;
     }
@@ -15,13 +18,19 @@ import java.util.Date;
         return name;
     }
     
-    public Date getBirthdate (){
+    public Calendar getBirthdate (){
         return birthdate;
     }
     
     public int getAge(){
-        int x = (int) ((new Date().getTime()-birthdate.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        int x = (int) (MSToYear(today.getTimeInMillis()-
+                birthdate.getTimeInMillis()));
         return x;
     }
+    
+    private long MSToYear(long MS) {
+     return MS/MS_per_year;
+ }
 }
 
